ba7a6fba9f0e427a533631f6c2a04fd9b5bccb4d
tliam1 <tliam@hawaii.edu>
2022-05-09 14:42:41 -1000

    Timing system and tweaks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-
+import time
+from time import sleep
 TOKEN = 'OTczMDg1MjI2NTM4NTIwNTg3.GRUbFl.3HoTV2kLiYMbjL8xQYZqz3VS2GlT1UBVkxAdM4'
 
 client = discord.Client()
@@ -11,12 +12,13 @@ board = [[1, 1, False], [2, 1, False], [3, 1, False],
 tiles = [' ', ' ', ' ',
          ' ', ' ', ' ',
          ' ', ' ', ' ']
-
+players = ["", ""]
+timeSinceLastPlay = 0
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    message = f"```{getTurn()}\n{printBoard()}```"
+    message = f"```Type !p to to join a queue or !help for more info!\n\n\n{printBoard()}```"
     text_channel_list = []
     for server in client.guilds:
         for channel in server.channels:
@@ -39,24 +41,45 @@ async def on_message(message):
     print(len(user_message))
 
     if user_message == "!help" and message.channel.name == 'tictactoe' and message.author != client.user:
-        await message.channel.send(f'Type coordinates using !x,y')
+        await message.channel.send(f'```Type !p to join queue.\nWhen the game starts type coordinates '
+                                   f'using !x,y to place an X or an O.\nIf an active game or queue runs over 2 minutes, '
+                                   f'you can end the game/queue with !r\n\n```')
+
+    if user_message == "!r" and message.channel.name == 'tictactoe' and message.author != client.user:
+        await message.channel.send(f"{checkTime()}")
+
+    if user_message == "!p" and message.channel.name == 'tictactoe' and message.author != client.user:
+        if players[0] == "":
+            players[0] = username
+            listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
+            listOfGlobals['timeSinceLastPlay'] = time.time()  # starts the timer for queue and game length
+            await message.channel.send(f'```{username}, you are X, waiting on O```')
+            return
+        elif players[1] == "" and username != players[0]:
+            players[1] = username
+            listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
+            listOfGlobals['timeSinceLastPlay'] = time.time()  # resets the queue reset timer when game actually starts
+            await message.channel.send(f"```{username}, you are O. Game can now be played\n\n{getTurn(players[0])}\n{printBoard()}```")
 
     if len(user_message) != 4 or user_message[0] != '!' or not user_message[1].isdigit() or \
             not user_message[3].isdigit() or int(user_message[1]) < 1 or int(user_message[1]) > 3 \
             or message.author == client.user or int(user_message[3]) < 1 or int(user_message[3]) > 3 \
-            or user_message[2] != ',':
+            or user_message[2] != ',' or players[0] == "" or players[1] == ""\
+            or not checkUsernameMatch(username):
         return
     if message.channel.name != 'tictactoe':
         return
     print(f'{username}: {user_message} ({channel})')
     boardPosition = [int(user_message[1]), int(user_message[3]), False]
-    checkIfPositionIsTaken(boardPosition)
+    if not checkIfPositionIsTaken(boardPosition):
+        await message.channel.send(f"```Coordinate Is Taken Please Try Again\n```")
 
     if checkWinCondition() or checkDrawCondition():  # the game is over and you need to reset board
-        await message.channel.send(f"```{printBoard()}\n {resetBoard()}\n\n\n```")
+        await message.channel.send(f"```{printBoard()}\n{resetBoard()}\n\n\n Use !p To Start A New Game!```")
+        return
 
     #  we are now able to send something to the user (This ends the event)
-    await message.channel.send(f"```{getTurn()}\n{printBoard()}```")
+    await message.channel.send(f"```{getTurn(username)}\n\n{printBoard()}```")
 
 
 # checks if tile is taken, if not place an x or o there depending on the turn
@@ -114,20 +137,23 @@ def checkDrawCondition():
 def resetBoard():
     # Game Over printing
     if checkDrawCondition():
-        gameOverText = "GAME OVER! DRAW!"
+        gameOverText = "```GAME OVER! DRAW!```"
         # refresh the screen, board, and tiles if I want a gameplay loop
-    elif isXTurn:
-        gameOverText = "GAME OVER O Wins"
+    elif isXTurn and checkWinCondition():
+        gameOverText = "```GAME OVER O Wins```"
+    elif not isXTurn and checkWinCondition():
+        gameOverText = "```GAME OVER X Wins```"
     else:
-        gameOverText = "GAME OVER X Wins"
+        gameOverText = f"```Restarting Game...\n\n Type !p To Start A New Game!```"
     # reset the board variables
     for i in range(len(tiles)):
         tiles[i] = ' '
     for i in range(len(board)):
         board[i][2] = False
-        print(board)
     # board[i] = [i, False]
-
+    for i in range(len(players)):  # resets active players
+        players[i] = ""
+        print(players[i])
     listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
     listOfGlobals['isXTurn'] = True  # resetting first player
     return gameOverText
@@ -139,11 +165,27 @@ def printBoard():
                 f"\t\n_____________________\n3 {tiles[6]}\t| {tiles[7]}\t| {tiles[8]}\t"
     return boardText
 
-def getTurn():
-    if isXTurn:
-        return "X's Turn"
-    else:
-        return "O's Turn"
+def getTurn(usernameStr):
+    print(usernameStr)
+    return f"{usernameStr}'s Turn"
+
+def checkUsernameMatch(username):
+    if (username == players[0] and isXTurn) or (username == players[1] and not isXTurn):
+        return True
+    return False
+
+def checkTime():
+    listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
+    returnText = ""
+    if listOfGlobals['timeSinceLastPlay'] == 0:
+        returnText = "No Currently Active Game\n"
+    elif abs(listOfGlobals['timeSinceLastPlay'] - time.time()) < 120:
+        returnText = f"Time Till Game Ends: {round(120 - (abs(listOfGlobals['timeSinceLastPlay'] - time.time())), 1)} " \
+                     f"seconds\nActive Players:\nX's: {players[0]}\nO's: {players[1]}\n"
+    elif abs(listOfGlobals['timeSinceLastPlay'] - time.time()) > 120:
+        returnText = f"{resetBoard()}"
+
+    return returnText
 
 
 client.run(TOKEN)
