a924126f73d08ee0c8e6688d3de825cd46de74a6
tliam1 <tliam@hawaii.edu>
2022-05-10 16:23:16 -1000

    converted system to discord emoji's

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ TOKEN = 'OTczMDg1MjI2NTM4NTIwNTg3.GRUbFl.3HoTV2kLiYMbjL8xQYZqz3VS2GlT1UBVkxAdM4'
 client = discord.Client()
 
 isXTurn = True
-board = [[1, 1, False], [2, 1, False], [3, 1, False],
-         [1, 2, False], [2, 2, False], [3, 2, False],
-         [1, 3, False], [2, 3, False], [3, 3, False]]
-tiles = [' ', ' ', ' ',
-         ' ', ' ', ' ',
-         ' ', ' ', ' ']
+board = [[1, False], [2, False], [3, False],
+         [4, False], [5, False], [6, False],
+         [7, False], [8, False], [9, False]]
+tiles = [':white_large_square:', ':white_large_square:', ':white_large_square:',
+         ':white_large_square:', ':white_large_square:', ':white_large_square:',
+         ':white_large_square:', ':white_large_square:', ':white_large_square:']
 players = ["", ""]
 playerIDs = [0, 0]
 timeSinceLastPlay = 0
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    message = f"```Type !p to to join a queue or !help for more info!\n\n\n{printBoard()}```"
+    message = f"```Type !p to to join a queue or !help for more info!\n\n\n```{printBoard()}"
     text_channel_list = []
     for server in client.guilds:
         for channel in server.channels:
@@ -40,8 +40,10 @@ async def on_message(message):
     print(len(user_message))
 
     if user_message == "!help" and message.channel.name == 'tictactoe' and message.author != client.user:
-        await message.channel.send(f'```Type !p to join queue.\nWhen the game starts type coordinates '
-                                   f'using !x,y to place an X or an O.\nIf an active game or queue runs over 2 minutes, '
+        await message.channel.send(f'```Type !p to join queue.\nWhen the game starts use numbers 1-9 '
+                                   f'using !(1-9) to place an X or an O.'
+                                   f' Number increases left to right, top to bottom'
+                                   f'\nIf an active game or queue runs over 2 minutes, '
                                    f'you can end the game/queue with !r\n\n```')
 
     if user_message == "!r" and message.channel.name == 'tictactoe' and message.author != client.user:
@@ -60,27 +62,28 @@ async def on_message(message):
             playerIDs[1] = message.author.id
             listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
             listOfGlobals['timeSinceLastPlay'] = time.time()  # resets the queue reset timer when game actually starts
-            await message.channel.send(f"```{username}, you are O.\n\n```" + f"<@{playerIDs[0]}> <@{playerIDs[1]}>" + f"```\nYour game can now be played\n\n{getTurn()}\n{printBoard()}```")
-
-    if len(user_message) != 4 or user_message[0] != '!' or not user_message[1].isdigit() or \
-            not user_message[3].isdigit() or int(user_message[1]) < 1 or int(user_message[1]) > 3 \
-            or message.author == client.user or int(user_message[3]) < 1 or int(user_message[3]) > 3 \
-            or user_message[2] != ',' or players[0] == "" or players[1] == ""\
+            await message.channel.send(f"```{username}, you are O.\n\n```" + f"<@{playerIDs[0]}> <@{playerIDs[1]}>" + f"```\nYour game can now be played\n\n{getTurn()}\n```{printBoard()}")
+    len(user_message)
+    if len(user_message) != 2 or user_message[0] != '!' or not user_message[1].isdigit()\
+            or int(user_message[1]) < 1 or int(user_message[1]) > 9 \
+            or message.author == client.user \
             or not checkUsernameMatch(username):
         return
     if message.channel.name != 'tictactoe':
         return
+
     print(f'{username}: {user_message} ({channel})')
-    boardPosition = [int(user_message[1]), int(user_message[3]), False]
+    boardPosition = [int(user_message[1]), False]
+    print(boardPosition)
     if not checkIfPositionIsTaken(boardPosition):
         await message.channel.send(f"```Coordinate Is Taken Please Try Again\n```")
 
     if checkWinCondition() or checkDrawCondition():  # the game is over and you need to reset board
-        await message.channel.send(f"```{printBoard()}\n{resetBoard()}\n\n\n Use !p To Start A New Game!```")
+        await message.channel.send(f"{printBoard()}\n```{resetBoard()}\n\n\n Use !p To Start A New Game!```")
         return
 
     #  we are now able to send something to the user (This ends the event)
-    await message.channel.send(f"```{getTurn()}\n\n{printBoard()}```")
+    await message.channel.send(f"```{getTurn()}```\n\n{printBoard()}")
 
 
 # checks if tile is taken, if not place an x or o there depending on the turn
@@ -88,14 +91,14 @@ def checkIfPositionIsTaken(pos):
     for i in range(9):
         if pos == board[i]:
             print("MATCH")
-            board[i] = [pos[0], pos[1], True]
+            board[i] = [pos[0], True]
             print(board[i])
             if isXTurn:
-                tiles[i] = 'x'
+                tiles[i] = ':x:'
                 listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
                 listOfGlobals['isXTurn'] = False
             elif not isXTurn:
-                tiles[i] = 'o'
+                tiles[i] = ':blue_circle:'
                 listOfGlobals = globals()  # only way to assign new values to "Undetermined value" globals
                 listOfGlobals['isXTurn'] = True
             return True
@@ -124,14 +127,14 @@ def checkWinCondition():
     return False
 
 def filled_with_Os(line):  # I like these declared outside of the scope of another function, more like c++
-    return line == ['o', 'o', 'o']  # returns true of an line in lines has a list containing 3 o's
+    return line == [':blue_circle:', ':blue_circle:', ':blue_circle:']  # returns true of an line in lines has a list containing 3 o's
 def filled_with_Xs(line):
-    return line == ['x', 'x', 'x']  # returns true of an line in lines has a list containing 3 X's
+    return line == [':x:', ':x:', ':x:']  # returns true of an line in lines has a list containing 3 X's
 
 # checks if all tiles are taken up
 def checkDrawCondition():
     for i in range(len(tiles)):
-        if tiles[i] == ' ':
+        if tiles[i] == ':white_large_square:':
             return False
     return True
 
@@ -141,16 +144,16 @@ def resetBoard():
         gameOverText = "GAME OVER! DRAW!"
         # refresh the screen, board, and tiles if I want a gameplay loop
     elif isXTurn and checkWinCondition():
-        gameOverText = "GAME OVER O Wins"
+        gameOverText = f"GAME OVER {players[1]} Wins"
     elif not isXTurn and checkWinCondition():
-        gameOverText = "GAME OVER X Wins"
+        gameOverText = f"GAME OVER {players[0]} Wins"
     else:
         gameOverText = f"Restarting Game...\n\n Type !p To Start A New Game!"
     # reset the board variables
     for i in range(len(tiles)):
-        tiles[i] = ' '
+        tiles[i] = ':white_large_square:'
     for i in range(len(board)):
-        board[i][2] = False
+        board[i][1] = False
     # board[i] = [i, False]
     for i in range(len(players)):  # resets active players
         players[i] = ""
@@ -163,9 +166,9 @@ def resetBoard():
 
 
 def printBoard():
-    boardText = f"   1       2      3\n1 {tiles[0]}\t| {tiles[1]}\t| {tiles[2]}"\
-                f"\t\n_____________________\n2 {tiles[3]}\t| {tiles[4]}\t| {tiles[5]}"\
-                f"\t\n_____________________\n3 {tiles[6]}\t| {tiles[7]}\t| {tiles[8]}\t"
+    boardText = f"{tiles[0]}{tiles[1]}{tiles[2]}\n"\
+                f"{tiles[3]}{tiles[4]}{tiles[5]}\n"\
+                f"{tiles[6]}{tiles[7]}{tiles[8]}"
     return boardText
 
 def getTurn():
